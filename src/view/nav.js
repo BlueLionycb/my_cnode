3e212184f4eca457536c1fd9c77b18498406ebc1
yichengbo903 <yichengbo903@lu.com>
2019-04-01 22:30:53 +0800

    首页link

--- src/view/nav.js
+++ src/view/nav.js
@@ -13,7 +13,7 @@ export default class Nav extends Component {
         return(
             <Menu mode={mode} id={id} theme ="light">
                 <Menu.Item>
-                    <Link to="/index"><Icon type="home"></Icon> 首页</Link>
+                    <Link to="/index/all"><Icon type="home"></Icon> 首页</Link>
                 </Menu.Item>
                 <Menu.Item>
                     <Link to="/book"><Icon type="book"></Icon>教程</Link>
